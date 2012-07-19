6fee79c785d5d634fd85ce5a4504f05ec7fc2d71
NatashaMurashev <nmurashev@gmail.com>
2012-07-19 15:45:04 -0700

    updated gem file

--- app/models/url.rb
+++ app/models/url.rb
@@ -6,20 +6,13 @@ class Url < ActiveRecord::Base
   before_save :set_short_url_slug
   
   def random_string
-    (0..5).map{65.+(rand(25)).chr}.join.downcase!
+    slug = (0..5).map{65.+(rand(25)).chr}.join.downcase!
+    return slug if Url.find_by_short_url_slug(slug).nil?
+    random_string
   end
   
   def set_short_url_slug
-    slug = random_string
-    if Url.all_short_url_slugs.include?(slug) 
-      set_short_url_slug
-    else
-      self.short_url_slug ||= random_string
-    end
-  end
-  
-  def self.all_short_url_slugs
-    Url.select(:short_url_slug).collect { |url| url.short_url_slug }
+    self.short_url_slug ||= random_string
   end
   
 end
